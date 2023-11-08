f0b4ba32623f306c662816ee27e7ece9d18571a2
midgitg1314 <midgitg14@gmail.com>
2023-11-08 16:01:00 -0700

    finished the project

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("Red", 3);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("Blue", 4,5);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle("Green", 6);
+        shapes.Add(circle1);
+
+        foreach  (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color}: shape has an area of {area}");
+        }
     }
 }
